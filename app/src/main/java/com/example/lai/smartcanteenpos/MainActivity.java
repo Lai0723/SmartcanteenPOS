bbdf6e999ab1d2bdaae603033ced2c9b56073970
GabGabGabGab <archangel4321@hotmail.com>
2017-11-27 00:11:55 +0800

    -Some fine tuning

--- app/src/main/java/com/example/lai/smartcanteenpos/MainActivity.java
+++ app/src/main/java/com/example/lai/smartcanteenpos/MainActivity.java
@@ -100,8 +100,7 @@ public class MainActivity extends AppCompatActivity {
                         fragment = fragmentWallet.newInstance();
                         break;
                     case R.id.action_fund:
-                        //checkCard(MainActivity.this, "https://gabriellb-wp14.000webhostapp.com/select_user.php");
-                        checkCard(MainActivity.this, "https://martpay.000webhostapp.com/gab_select_user.php");
+                        //checkCard(MainActivity.this, "https://martpay.000webhostapp.com/gab_select_user.php");
                         fragment = fragmentFund.newInstance();
                         break;
                     case R.id.action_wallet_history:
